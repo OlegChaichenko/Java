2307a89fa6adb21d49359e0f7c0db43d5c354d30
OlegChaichenko <113429945+OlegChaichenko@users.noreply.github.com>
2023-12-03 10:35:26 +0200

    Chaichenko Oleg comp mat1 CW11 1.1-1.6
    Chaichenko Oleg comp mat1 CW11 1.1-1.6

---
+++ task5.java
@@ -0,0 +1,13 @@
+package kr11;
+
+import java.util.Scanner;
+
+public class task5 {
+    static public void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine()
+                .replace("small", "very small")
+                .replace("large", "very large");
+        System.out.println(str);
+    }
+}
